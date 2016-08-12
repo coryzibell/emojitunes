f8f1c3a7b77ed42bc5ccddb01e60c3f0a55f44bf
Adam Chambers <adammchambers@gmail.com>
2016-08-12 01:43:34 -0400

    Error handling on both JSON and browser routes

--- index.js
+++ index.js
@@ -1,42 +1,55 @@
+// require http, routing modules
 const http = require('http');
 const routes = require('patterns')();
-const SpotifyWebApi = require('spotify-web-api-node');
-const _ = {
-	forOwn: require('lodash/forOwn'),
-	shuffle: require('lodash/shuffle'),
-};
 
+// require spotify and initialise Spotify
+const SpotifyWebApi = require('spotify-web-api-node');
 const spotifyApi = new SpotifyWebApi({
 	clientId: '6c926cf5c6cd4f2ead716ce1b20d9ef7',
 	clientSecret: '36a13c94552449a79f856abfeabc0455',
 });
 
+// lodash helper functions
+const _ = {
+	forOwn: require('lodash/forOwn'),
+	shuffle: require('lodash/shuffle'),
+};
+
+// object of genres, each with an array of emojis
 const genres = require('./lib/genres');
 
+// fetch spotify access token
+// clientCredentials method does not require user authentication
 spotifyApi.clientCredentialsGrant().then(data => {
 	spotifyApi.setAccessToken(data.body.access_token);
-}, err => {
-	console.log('Something went wrong when retrieving an access token', err);
+}, error => {
+	console.log('Error retrieving access token', error);
 });
 
+// get recommendations API route
+// @params emoji e.g 🤘
+// @return JSON object containing genre and track arrays
 routes.add('GET /recommendations/{emoji}', (req, res) => {
 	res.setHeader('Content-Type', 'application/json');
-	getRecommendations(req).then(
+	getRecommendations(decodeURIComponent(req.params.emoji)).then(
 		recommendations => res.end(JSON.stringify(recommendations)),
 		error => res.end(JSON.stringify(error))
 	);
 });
 
+// get recommendations and return grid of Spotify play button iframes
 routes.add('GET /recommendations-browser/{emoji}', (req, res) => {
 	res.setHeader('Content-Type', 'text/html');
-	getRecommendations(req).then(recommendations => {
-		if (!recommendations.tracks && recommendations.tracks.length) {
+	getRecommendations(decodeURIComponent(req.params.emoji)).then(recommendations => {
+		// no tracks found
+		if (!recommendations.tracks.length) {
 			res.end('Nothing found 😞');
 			return;
 		}
 
 		const output = [];
 
+		// loop through recommendations and build up array of iframes
 		recommendations.tracks.forEach(track => {
 			output.push(`
 				<iframe src="https://embed.spotify.com/?uri=${track.url}"
@@ -49,6 +62,7 @@ routes.add('GET /recommendations-browser/{emoji}', (req, res) => {
 			`);
 		});
 
+		// render flex box grid of iframes with title of comma separated genres
 		res.end(`
 			<h1 style="text-align: center; text-transform: capitalize;">${recommendations.genres.join(', ')}</h1>
 			<div style="width: 1200px; margin: 0 auto; display: flex; flex-wrap: wrap;">
@@ -56,32 +70,41 @@ routes.add('GET /recommendations-browser/{emoji}', (req, res) => {
 			</div>
 		`);
 	}, error => {
-		console.log(error);
-		res.end('There was an error 😢');
+		console.log('Error fetching recommendations', error);
+		res.end('No genres match emoji');
 	});
 });
 
+// create server
 const server = http.createServer((req, res) => {
+	// match routes
 	const match = routes.match(`${req.method} ${req.url}`);
 
+	// if match found call function
 	if (match) {
 		const fn = match.value;
 		req.params = match.params;
 		fn(req, res);
+	} else {
+		res.end('404');
 	}
 });
 
-// listen for http request on port 9090
+// listen for http request on port 9000
 server.listen(9000, () => {
-	console.log('Server is running on http://localhost:9000');
+	console.log('🤘 Server is running on http://localhost:9000 🤘');
 });
 
-function getRecommendations(req) {
-	const emoji = decodeURIComponent(req.params.emoji);
+// find genres matching emoji and fetch recommendations from Spotify
+// @params emoji e.g 🤘
+function getRecommendations(emoji) {
 	const foundGenres = [];
 
+	// loop through genres object
 	_.forOwn(genres, (emojis, genre) => {
+		// loop through genre's emojis
 		emojis.every(g => {
+			// if emoji found add genre to foundGenres and break loop
 			if (g === emoji) {
 				foundGenres.push(genre);
 				return false;
@@ -91,21 +114,27 @@ function getRecommendations(req) {
 		});
 	});
 
+	// shuffle the genres we found
 	const shuffledFoundGenres = _.shuffle(foundGenres).slice(0, 4);
 
 	console.log(shuffledFoundGenres);
 
-	if (!foundGenres.length) {
-		return false;
-	}
-
+	// return promise and wait for Spotify API call
 	return new Promise((resolve, reject) => {
+		if (!foundGenres.length) {
+			reject({
+				'error': 'No genres match emoji',
+			});
+		}
+
+		// fetch recommendations from spotify using shuffle genres found above
 		spotifyApi.getRecommendations({
 			seed_genres: shuffledFoundGenres,
 			min_popularity: 50,
 		}).then(data => {
 			const tracks = [];
 
+			// loop through each track and add object containing artist, title, url
 			data.body.tracks.forEach(track => {
 				tracks.push({
 					artist: track.artists[0].name,
@@ -114,12 +143,17 @@ function getRecommendations(req) {
 				});
 			});
 
+			// resolve promise and return shuffled genres and tracks
 			resolve({
 				genres: shuffledFoundGenres,
 				tracks,
 			});
+		// error fetching recommendations from Spotify
+		// reject promise
 		}, error => {
-			reject(error);
+			reject({
+				error,
+			});
 		});
 	});
 }
