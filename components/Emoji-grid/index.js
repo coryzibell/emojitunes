59723345b91d4478b0b2c7bf85df79c979850d02
Adam Chambers <adammchambers@gmail.com>
2017-03-18 14:56:23 -0400

    Set up keypress on body for keyboard navigation through emojis

--- components/Emoji-grid/index.js
+++ components/Emoji-grid/index.js
@@ -12,6 +12,12 @@ export default class Search {
     this.container.appendChild(this.list)
 
     this.search.addEventListener('keyup', e => this.filterEmojis(e))
+
+    document.body.addEventListener('keydown', e => {
+      if (e.keyCode === 13) {
+        console.log(e)
+      }
+    })
   }
 
   addEmojiNode(emoji) {
@@ -20,7 +26,7 @@ export default class Search {
     child.classList.add('Emoji-grid__item')
     child.setAttribute('data-emoji', emojiName)
     child.innerHTML = emojiCode
-    child.addEventListener('click', e => this.getRecommendations(e.currentTarget.innerHTML))
+    child.addEventListener('click', e => this.triggerEmoji(e.currentTarget))
 
     this.list.appendChild(child)
   }
@@ -33,6 +39,10 @@ export default class Search {
         item.classList.remove('Emoji-grid__item--hide')
       }
     })
+  }
+
+  triggerEmoji(emojiNode) {
+    this.getRecommendations(emojiNode.innerHTML)
   }
 
   getRecommendations(emoji) {
