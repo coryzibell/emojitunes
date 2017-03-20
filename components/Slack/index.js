9ee599a6229d92514f368d16889c8a35e9ebe2c8
Adam Chambers <adammchambers@gmail.com>
2017-03-19 23:34:40 -0400

    Add letters animation to slack component

--- components/Slack/index.js
+++ components/Slack/index.js
@@ -7,6 +7,10 @@ export default class Slack {
   startConversation() {
     setTimeout(() => {
       this.msgs[0].classList.add('Slack__row--first-animate')
+
+      setTimeout(() => {
+        this.typeMsg(this.msgs[0].querySelector('[data-msg]'), '🤘')
+      }, 500)
     }, 4000)
 
     setTimeout(() => {
@@ -14,7 +18,47 @@ export default class Slack {
 
       setTimeout(() => {
         this.msgs[1].classList.add('Slack__row--second-animate')
+        setTimeout(() => {
+          this.typeMsg(this.msgs[1].querySelector('[data-msg]'), '👊')
+          setTimeout(() => this.addIframe(this.msgs[1].querySelector('[data-msg]')), 1000)
+        }, 500)
       }, 1000)
     }, 6000)
+  }
+
+  typeMsg(msg, emoji) {
+    const letters = `
+      <span class="Slow__msg-part">
+      ${msg.innerHTML.split('').join('</span><span class="Slow__msg-part">')}
+      </span>
+      <span class="Slow__msg-part">${emoji}</span>
+    `
+    msg.innerHTML = letters
+    msg.style.opacity = 1
+
+    const parts = msg.querySelectorAll('.Slow__msg-part')
+    parts.forEach((part, index) => {
+      setTimeout(function() {
+        this.style.opacity = 1
+      }.bind(part), index * 30)
+    })
+  }
+
+  addIframe(msg) {
+    const container = document.createElement('div')
+    const html = `
+      <iframe
+        class="Slack__spotify"
+        src="https://embed.spotify.com/?uri=https://open.spotify.com/track/2SgbR6ttzoNlCRGQOKjrop"
+        width="150"
+        height="190"
+        frameborder="0"
+        style="border: 0;"
+        allowtransparency="true">
+      </iframe>
+    `
+
+    container.innerHTML = html
+    msg.parentNode.appendChild(container)
   }
 }
